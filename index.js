995a250429db4979492f9e398f32336ec053fe56
Alvinlz <me@alvinl.com>
2014-08-25 21:53:29 -0400

    Add comments

--- index.js
+++ index.js
@@ -5,18 +5,22 @@ module.exports = function (Stem) {
       host  = Stem.config.redisHost || 'localhost',
       redis = require('redis').createClient(port, host);
 
+  // Wait until Redis is ready to start taking commands
   redis.on('ready', function () {
 
+    // Fetch previous storage
     redis.get('stem:' + Stem.config.username, function (err, data) {
 
       if (err)
         return Stem.log.error('Error fetching storage data from Redis:', err.message);
 
+      // If previous storage was found, parse it and initialize it
       if (data)
         Stem.storage._data = JSON.parse(data);
 
     });
 
+    // Save new data to redis
     Stem.on('save', function (data) {
 
       redis.set('stem:' + Stem.config.username, JSON.stringify(data), function (err) {
